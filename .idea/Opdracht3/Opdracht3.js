e24766bf6443ee3bbf34169ea307a19d2760cece
Boaz <mr.boaz@hotmail.com>
2019-11-29 10:55:55 +0100

    first

--- .idea/Opdracht3/Opdracht3.js
+++ .idea/Opdracht3/Opdracht3.js
@@ -3,35 +3,30 @@ function Winning(){
     var result;
     for (var i = 0, length = radios.length; i < length; i++){
      if (radios[i].checked){
-           i = convert(i+1);
-           cpu = convert(Math.floor(Math.random() * (3) ) + 1);
-      if (cpu == i){
-       result = "Gelijkspel! Jij en de computer kozen beide "+ cpu.toLowerCase();
-      }
-      if ((i == "Steen" && cpu == "Schaar") || (i == "Papier" && cpu == "Steen") || (i == "Schaar" && cpu == "Papier")){
-       result = "Je hebt gewonnen! " + i + " wint van " + cpu.toLowerCase();
-      }
-      if ((i == "Schaar" && cpu == "Steen") || (i == "Steen" && cpu == "Papier") || (i == "Papier" && cpu == "Schaar")){
-        result = "Computer wint! " + cpu + " wint van " + i.toLowerCase();
-      }
-      break;
-      }
+        i = convert(i+1);
+        cpu = convert(Math.floor(Math.random() * (3) ) + 1);
+        if (cpu == i){
+            result = "Gelijkspel! Jij en de computer kozen beide "+ cpu.toLowerCase();
+        }
+        if ((i == "Steen" && cpu == "Schaar") || (i == "Papier" && cpu == "Steen") || (i == "Schaar" && cpu == "Papier")){
+            result = "Je hebt gewonnen! " + i + " wint van " + cpu.toLowerCase();
+        }
+        if ((i == "Schaar" && cpu == "Steen") || (i == "Steen" && cpu == "Papier") || (i == "Papier" && cpu == "Schaar")){
+            result = "Computer wint! " + cpu + " wint van " + i.toLowerCase();
+        }
+            break;
+        }
      }
      document.getElementById("result").innerHTML = result;
-     /*var delayInMilliseconds = 10000; //10 seconds
-
-                 setTimeout(function() {
-                 location.reload();
-                 }, delayInMilliseconds);*/
     }
 function convert(number){
     if (number == 1){
-    return "Steen";
+        return "Steen";
     }
     if (number == 2){
-    return "Papier";
+        return "Papier";
     }
     if (number == 3){
-    return "Schaar";
+        return "Schaar";
     }
 }
